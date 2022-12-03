5c1c1ddd81d87575f7e4353d1297d68bdeaf23f7
Minwoo <minwooj40@gmail.com>
2022-12-03 15:38:32 +0800

    Detection + Marking + Localization

--- src/image_detect/src/image_detect.py
+++ src/image_detect/src/image_detect.py
@@ -21,14 +21,14 @@ class detectionModel:
 
 #########################################################
         #Image features
-        self.orb_cnt = self.square_cnt = [0] * len(self.pictures)
+        self.detectionCnt = [0] * len(self.pictures)
         self.orb = cv2.ORB_create()
         self.keyPoints, self.descriptors = [], []
 
         for i, picture in enumerate(self.pictures):
             imgSize = (400, 400)
             # picture = cv2.resize(picture, imgSize, interpolation = cv2.INTER_AREA)
-            picture = cv2.flip(picture, 1)
+            if(i == 0 or i == 1 or i == 3): picture = cv2.flip(picture, 1)
             keyPoint, descriptor = self.orb.detectAndCompute(picture, None)
             self.keyPoints.append(keyPoint) 
             self.descriptors.append(descriptor)
@@ -56,7 +56,7 @@ class detectionModel:
 
             marker.scale.x = 0.5
             marker.scale.y = 0.5
-            marker.scale.z = 50.0
+            marker.scale.z = 0.5
             marker.id = i
 
             marker.lifetime = rospy.Duration(1000000)
@@ -80,7 +80,8 @@ class detectionModel:
                 dist.append([matches[0].distance, i])
 
             dist = sorted(dist)
-            if(dist[0][0] < 30.0):
+            #print(dist[0][0], dist[0][1])
+            if(dist[0][0] < 15.0):
                 # print("Detect the image and inference the best fit", dist[0][1])
                 return dist[0][1]
             else:
@@ -88,49 +89,27 @@ class detectionModel:
                 return -1
         
 
-
-        # try:
-        #     dist = []
-        #     keyPoint = self.orb.detect(img, None)
-        #     keyPoint, descriptor = self.orb.compute(img, keyPoint)
-
-        #     #cv2.NORM_HAMMING
-        #     for i, des in enumerate(self.descriptors):
-        #         bf = cv2.BFMatcher(cv2.NORM_HAMMING)
-        #         matches = bf.match(des, descriptor)
-        #         dist.append(matches.distance, i)
-
-        #     dist = sorted(dist)
-        #     print("Detect the image and inference the best fit", dist[0][1])
-        #     return dist[0][1]
-        # except:
-        #     print("ERRRRRROROROROROROROROROROROROROR")
-        #     return -1
-        
-
     def marking(self, id, img):
-        # print("Found the image and start to mark")## work
-        img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        _, img_BW = cv2.threshold(img_gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
-        contours, hierarchy = cv2.findContours(img_BW, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+        grayImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        _, grayImg = cv2.threshold(grayImg, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+        grads, _ = cv2.findContours(grayImg, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
-        x, y, w, h = cv2.boundingRect(max(contours, key=cv2.contourArea))
+        x, y, w, h = cv2.boundingRect(max(grads, key = cv2.contourArea))
+
+
+        ###### Ratio Test
         ratio = float(w)/h
-
-        if ratio > 1.1 and ratio < 0.5:
-            self.square_cnt[id] = 0
+        if ratio >= 0.5 and ratio <= 1.1:
+            self.detectionCnt[id] += 1
         else:
-            self.square_cnt[id] += 1
+            self.detectionCnt[id] = 0
 
-        if (self.square_cnt[id] >= 15):
+        if (self.detectionCnt[id] >= 15):
             self.markers[id].pose.position.x = 1 / math.tan(math.pi / 8 * h / img.shape[1]) * 0.5
             self.markers[id].pose.position.y = (x + w/2) / img.shape[1]
-            self.markers[id].pose.position.z = 3
-
+            self.markers[id].pose.position.z = 0
             self.publisher.publish(self.markers[id])
             self.marked[id] = True
-            #return
-
 
     def callback(self, img):
     
@@ -141,9 +120,6 @@ class detectionModel:
             if(self.marked[bestId] == False):
                 # print("Found image start marking")
                 self.marking(bestId, img)
-            else:
-                a = 5
-                # print("already marked")
 
 def main():
     rospy.init_node("image_detect")
---
+++ src/localization/src/localization.py
@@ -0,0 +1,48 @@
+#! /usr/bin/env python
+import rospy
+from std_msgs.msg import String
+from geometry_msgs.msg import PoseStamped
+
+coord = {
+    "A": ((5, 0), (-3.5, -3.5)),
+    "B": ((5, -3.5), (-3.5, -7)),
+    "C": ((5, -7), (-3.5, -14)),
+    "D": ((10.5, 0), (5, -14)),
+}
+
+class AreaIdentifier:
+    def __init__(self):
+        self.publisher = rospy.Publisher("/area", String, queue_size=1)
+        self.subscriber = rospy.Subscriber("/slam_out_pose", PoseStamped, self.callback, queue_size=1)
+        self.area = "START"
+
+    def callback(self, msg_in):
+        x = msg_in.pose.position.x
+        y = msg_in.pose.position.y
+        print(x, y)
+        curArea = self.area
+
+        for a, coor in coord.items():
+            if self.areaCheck(x, y, coor):
+                curArea = a
+                break
+
+        if curArea != self.area:
+            self.area = curArea
+            self.publisher.publish(curArea)
+            print("I AM AT", curArea, "+++++++++++++++++++++++++++++++++++++++++++++")
+            
+
+    def areaCheck(self, x, y, coor):
+        return coor[0][0] >= x and x >= coor[1][0] and coor[0][1] >= y and y >= coor[1][1]
+
+def main():
+    rospy.init_node("localization")
+    node = AreaIdentifier()
+    try:
+        rospy.spin()
+    except KeyboardInterrupt:
+        rospy.loginfo("KeyboardInterrupted")
+
+if __name__ == '__main__':
+    main()
